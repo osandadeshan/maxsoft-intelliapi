ee353a09769a2d070bd44f9f3db7f524630a422d
Osanda Deshan <osanda.deshan@gmail.com>
2018-03-29 13:03:03 +0530

    Renamed to IntelliAPI

--- reports/html-report/js/search_index.js
+++ reports/html-report/js/search_index.js
@@ -1,1 +1,1 @@
-var index = {"Tags":{"get_pi_token":["specs\\Access Token\\Get Production PI Token.html"],"production":["specs\\Access Token\\Get Production PI Token.html"]},"Specs":{"Get Production PI Token Specification":["specs\\Access Token\\Get Production PI Token.html"]}};
+var index = {"Tags":{"get_pi_token":["specs\\Access Token\\Get Staging PI Token.html"],"staging":["specs\\Access Token\\Get Staging PI Token.html"]},"Specs":{"Get Staging PI Token Specification":["specs\\Access Token\\Get Staging PI Token.html"]}};
