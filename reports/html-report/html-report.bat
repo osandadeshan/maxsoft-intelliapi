bd30c6401d19e66e67b0b6cd23b6a0e069456d12
Osanda Deshan <osanda.deshan@gmail.com>
2018-07-18 11:22:25 +0530

    Changed BarChart to show failed graphs first

--- reports/html-report/html-report.bat
+++ reports/html-report/html-report.bat
@@ -1,2 +1,2 @@
 @echo off 
-C:\Users\UnimaOs\AppData\Roaming\gauge\plugins\html-report\4.0.4\bin\html-report.exe %*
+C:\Users\unimaos\AppData\Roaming\gauge\plugins\html-report\4.0.4\bin\html-report.exe %*
