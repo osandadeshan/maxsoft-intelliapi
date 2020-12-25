b8f5ef4ee5d217787762059c3178bb3e3906aeb9
Osanda Deshan <osanda.deshan@gmail.com>
2020-12-25 20:38:20 +0530

    Updated pre_requisites_test with tag

--- bat_files/test_runner_bats/Dev BVT.bat
+++ bat_files/test_runner_bats/Dev BVT.bat
@@ -4,8 +4,8 @@ echo Executing MaxSoft IntelliAPI Dev BVT API Test Automation Suite.............
 cd ..
 cd .. 
 
-call mvn test-compile gauge:execute -DspecsDir="specs\Pre Test" -Denv="dev"
-call mvn test-compile gauge:execute -DspecsDir="specs\Access Token,specs\Data Stores,specs\Other" -Denv="dev" -DinParallel=true
+call mvn test-compile gauge:execute -DspecsDir="specs\pre_requisites_test" -Denv="dev"
+call mvn test-compile gauge:execute -DspecsDir="specs" -Denv="dev" -DinParallel=true
 
 call mvn clean -DemailConfigEnv=dev install exec:java
 echo Exit Code = %ERRORLEVEL%
