bcadac6e6a423d894b66cd316682ee470f546a98
Osanda Deshan <osanda.deshan@gmail.com>
2021-01-31 19:52:04 +0530

    Updated batch files

--- batch_files/dev_regression_run_with_greporter.bat
+++ batch_files/dev_regression_run_with_greporter.bat
@@ -4,7 +4,7 @@ echo Executing MaxSoft IntelliAPI Dev BVT API Test Automation Suite.............
 cd .. 
 
 call mvn test-compile gauge:execute -DspecsDir="specs" -Dtags="pre_requisites" -Denv="dev"
-call mvn test-compile gauge:execute -DspecsDir="specs" -Dtags="regression" -Denv="dev"
+call mvn test-compile gauge:execute -DspecsDir="specs" -Denv="dev"
 
 call mvn clean -DemailConfigEnv=dev install exec:java
 echo Exit Code = %ERRORLEVEL%
